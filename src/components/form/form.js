44d5e01ae981efbd6e6b3ec1a6855f727c734094
Guidogl <gvidasglebas@gmail.com>
2019-06-24 19:17:22 +0300

    Updated contact form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import "./form.css";
 
 export default function Form() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
 
   async function createNewProfile() {
     var formData = new FormData();
 
-    formData.append("firstName", firstName);
-    formData.append("lastName", lastName);
+    formData.append("name", name);
+    formData.append("phone", phone);
     formData.append("email", email);
 
     fetch("https://a81f7cb9.eu-gb.apiconnect.appdomain.cloud/countdown", {
@@ -24,30 +24,28 @@ export default function Form() {
 
   return (
     <form className="container" onSubmit={createNewProfile()}>
-      <label>
-        Name
-        <input
-          type="text"
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}
-        />
-      </label>
-      <label>
-        Last Name
-        <input
-          type="text"
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}
-        />
-      </label>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </label>
+      <span className="contact-form-title">TRAVEL TOGETHER</span>
+      <input
+        className="wrap-input"
+        placeholder="Your Name"
+        value={name}
+        type="text"
+        onChange={e => setName(e.target.value)}
+      />
+      <input
+        className="wrap-input"
+        type="text"
+        placeholder="Your Phone"
+        value={phone}
+        onChange={e => setPhone(e.target.value)}
+      />
+      <input
+        className="wrap-input"
+        type="text"
+        placeholder="Your E-mail"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
       <input type="submit" value="Submit" />
     </form>
   );
